79448f466ce1ee87e059eec8dd4fe39cb0726b1b
Yağmur <99952328+YamurBirinci@users.noreply.github.com>
2024-01-12 23:43:30 +0300

    database.sql

--- database.sql
+++ database.sql
@@ -12,7 +12,6 @@ CREATE TABLE IF NOT EXISTS user_info (
     user_phone varchar(10)
 );
 
-
 use group09;
 CREATE TABLE IF NOT EXISTS product_info (
     p_id int auto_increment primary key,
@@ -36,8 +35,6 @@ CREATE TABLE IF NOT EXISTS cart_info (
     p_image varchar(50) not null
 );
 
-
-
 use group09;
 CREATE TABLE IF NOT EXISTS order_info (
 	orderID varchar(50),
@@ -47,30 +44,11 @@ CREATE TABLE IF NOT EXISTS order_info (
     order_p_kg double not null,
     order_p_image varchar(50) not null,
     order_status varchar(50) not null,
-    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    order_carrierID int,
+    c_address varchar(200),
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    completedDelivery TIMESTAMP
 );
-
-UPDATE order_info SET orderID = CONCAT(order_customer_id, SUBSTRING(time FROM 1 FOR 4), SUBSTRING(time FROM 6 FOR 2), SUBSTRING(time FROM 9 FOR 2), SUBSTRING(time FROM 12 FOR 2), SUBSTRING(time FROM 15 FOR 2), SUBSTRING(time FROM 18 FOR 2)
-);
-
-SELECT order_customer_id, orderID, time, order_p_name, order_p_price, order_p_kg, order_status FROM order_info WHERE order_status = 'Order Received' ORDER BY time DESC;
-
-
-
-
-
-
-
-INSERT INTO order_info (order_customer_id, order_p_name, order_p_price, order_p_kg, order_p_image, order_status, time) SELECT customer_id, p_name, p_price, p_kg, p_image, 'Sipariş alındı', NOW() FROM cart_info WHERE customer_id = 1;
-
-select * from group09.order_info;
-delete from order_info;
-
-drop table product_info;
-
-SELECT order_customer_id, orderID, time, order_p_name, order_p_price, order_p_kg, order_status FROM order_info WHERE order_status = 'Order Received' ORDER BY time DESC;
-
-SELECT order_status from group09.order_info  where orderID= "120240112012524" limit 1;
 
 use group09;
 INSERT INTO product_info (p_name, p_type, p_price, p_stock, p_kg, p_image, p_threshold)
@@ -158,60 +136,11 @@ INSERT INTO user_info (username, user_name, user_surname, user_password, user_ro
 VALUES("Yamur61", "Yağmur Fatma", "Birinci", "Khas61.37", "customer", "Beylerbeyi mahallesi, Üsküdar", "5389855850");
 
 INSERT INTO user_info (username, user_name, user_surname, user_password, user_role, user_address, user_phone)
-VALUES("OykuSucu", "Öykü", "Sucuoğlu", "Kedimvar123.", "owner", "", "");
+VALUES("Oyku", "Oyku", "Sucuoglu", "oykuSucu1.", "owner", "", "");
 
--- select * from group09.user_info
--- select * from group09.product_info
--- select * from group09.cart_info
--- select * from group09.order_info
+INSERT INTO user_info (username, user_name, user_surname, user_password, user_role, user_address, user_phone)
+VALUES("Ozan", "Ozan", "Sahan", "Ozan1.", "carrier", "", "");
 
-drop table order_info;
+INSERT INTO user_info (username, user_name, user_surname, user_password, user_role, user_address, user_phone)
+VALUES("Helin", "Helin", "Arslan", "Helin1.", "owner", "", "");
 
-
-SELECT orderID, order_customer_id, time, order_p_name, order_p_price, order_p_kg, order_status
-FROM order_info
-WHERE order_customer_id = 1 AND order_status = 'Order Received'
-ORDER BY time DESC;
-
-    
-    
-SELECT customer_id, p_name, SUM(p_price) AS total_price, SUM(p_kg) AS total_kg, p_image FROM group09.cart_info WHERE customer_id = 1 GROUP BY customer_id, p_name, p_image HAVING total_price > 0;
-SELECT customer_id, p_name, p_price, p_kg, p_image FROM group09.cart_info WHERE customer_id = 1;
-
-
-SELECT customer_id, p_name, SUM(p_price) AS total_price, SUM(p_kg) AS total_kg, p_image FROM group09.cart_info WHERE customer_id = 1 and p_name = 'Banana' GROUP BY customer_id, p_name, p_image HAVING total_price > 0  order by p_name;
-
-SELECT customer_id, SUM(p_price) AS total_price FROM group09.cart_info WHERE customer_id = 1 GROUP BY customer_id  HAVING total_price > 0 ;
-
-
-DELETE FROM group09.order_info;
-
-SELECT * FROM order_info WHERE order_customer_id = 1 ;
-SELECT order_customer_id, time, order_status, SUM(order_p_price) AS total_price FROM order_info WHERE order_customer_id = 1 GROUP BY order_customer_id, time, order_status ORDER BY time DESC;
-SELECT order_customer_id, MAX(time) AS max_time, order_status, SUM(order_p_price) AS total_price FROM order_info WHERE order_customer_id = 1  GROUP BY order_customer_id, order_status ORDER BY max_time DESC;
-
-SELECT order_customer_id, CONCAT(order_customer_id, '_', time) AS orderID, time, order_p_name, order_p_price, order_p_kg, order_status FROM order_info WHERE order_status = 'Order Received' ORDER BY time DESC;
-
-select * from product_info where p_type = 'fruit' and p_stock > 0 order by p_name;
-select * from group09.product_info order by p_name;
-
-select * from order_info;
-
-SELECT order_customer_id, CONCAT(order_customer_id, '_', REGEXP_REPLACE(time, '[^0-9]', '')) AS orderID, time, order_p_name, order_p_price, order_p_kg, order_status FROM order_info WHERE order_status = 'Order Received' ORDER BY time DESC;
-
-
-UPDATE product_info SET p_stock = p_stock + 1 WHERE p_id = 2;
-
-UPDATE group09.product_info SET p_stock = 10 WHERE p_id = 1;
-SELECT order_customer_id, CONCAT(order_customer_id, REPLACE(time, '[^0-9]', '')) AS orderID, time, order_p_name, order_p_price, order_p_kg, order_status FROM order_info WHERE order_status = 'Order Received' ORDER BY time DESC;
-
-UPDATE group09.order_info SET order_status = 'Order Has Been Prepared' WHERE CONCAT(order_customer_id, '_', time) = '3_2024-01-10 20:34:20';
-
-SELECT order_customer_id, time, order_status, SUM(order_p_price) AS total_price FROM order_info WHERE order_customer_id = 1 GROUP BY order_customer_id, time, order_status ORDER BY time DESC;
-
-
-
-INSERT INTO order_info (order_customer_id, order_p_name, order_p_price, order_p_kg, order_p_image, order_status, time) SELECT customer_id, p_name, SUM(p_price) AS total_price, SUM(p_kg) AS total_kg, p_image, 'Order Received' AS order_status, NOW() AS time FROM group09.cart_info WHERE customer_id = 1 GROUP BY customer_id, p_name, p_image HAVING total_price > 0;
-
-
-SELECT order_customer_id, CONCAT(order_customer_id, CONCAT(SUBSTRING(time FROM 1 FOR 4), SUBSTRING(time FROM 6 FOR 2), SUBSTRING(time FROM 9 FOR 2), SUBSTRING(time FROM 12 FOR 2), SUBSTRING(time FROM 15 FOR 2), SUBSTRING(time FROM 18 FOR 2))) AS orderID, time, order_p_name, order_p_price, order_p_kg, order_status FROM order_info WHERE order_status = 'Order Received' ORDER BY time DESC;
